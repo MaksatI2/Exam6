5aef00906c974251b2f837c775dced7a463775a6
I.Maksat <maksat@MacBookAir.lan>
2025-03-08 15:46:27 +0600

    Added id field getId setId

--- src/model/Task.java
+++ src/model/Task.java
@@ -5,16 +5,26 @@ import enums.TaskType;
 import java.time.LocalDate;
 
 public class Task {
+    private String id;
     private String title;
     private String description;
     private TaskType type;
     private LocalDate date;
 
-    public Task(String title, String description, TaskType type, LocalDate date) {
+    public Task(String id,String title, String description, TaskType type, LocalDate date) {
+        this.id = id;
         this.title = title;
         this.description = description;
         this.type = type;
         this.date = date;
+    }
+
+    public  String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getTitle() {
